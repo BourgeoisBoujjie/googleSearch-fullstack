5528fb02b5b04d1b43d06633f05b9046e40a0418
Justin Bourgeois <99760766+BourgeoisBoujjie@users.noreply.github.com>
2022-07-05 12:47:53 +0700

    fixed some css issues

--- controllers/search.js
+++ controllers/search.js
@@ -1,9 +1,11 @@
 // Packages
 const express = require('express')
+const res = require('express/lib/response')
 const router = express.Router()
 
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
 
 // Export
+
 module.exports = router
