118605fed53fadda948d142dc90193b9ccb13c32
Justin Bourgeois <99760766+BourgeoisBoujjie@users.noreply.github.com>
2022-07-08 15:50:23 +0700

    connected to database and created model schema

--- models/results.js
+++ models/results.js
@@ -1,3 +1,25 @@
 const mongoose = require('mongoose')
+const { router } = require('../app')
 
 // Create the results moodel
+mongoose.model('products', {
+    title: { 
+        type: String,
+        required: true },
+    description: {
+        type: String,
+        required: true },
+    url: {
+        type: String,
+        required: true },
+    links: [{
+        title: {
+            type: String, 
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }]
+  })
